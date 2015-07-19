f738a9031b5ca7265c2d55e71401c579ac75be3d
Demircan Celebi <demircancelebi@gmail.com>
2015-07-19 12:40:42 +0300

    added errSrc directive

---
+++ client/app/errSrc/errSrc.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('garajApp')
+  .directive('errSrc', function () {
+    return {
+      link: function(scope, element, attrs) {
+
+        scope.$watch(function() {
+          return attrs['ngSrc'];
+        }, function (value) {
+          if (!value) {
+            element.attr('src', attrs.errSrc);
+          }
+        });
+
+        element.bind('error', function() {
+          element.attr('src', attrs.errSrc);
+        });
+      }
+    };
+  });
