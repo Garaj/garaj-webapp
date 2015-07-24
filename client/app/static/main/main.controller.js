5c2d51fa2ad927444f5616eba4d8bc133fcbafb9
Demircan Celebi <demircancelebi@gmail.com>
2015-07-24 16:40:30 +0300

    removed people from sponsors

--- client/app/static/main/main.controller.js
+++ client/app/static/main/main.controller.js
@@ -3,27 +3,4 @@
 angular.module('garajApp')
   .controller('MainCtrl', function ($scope, posts) {
     $scope.posts = posts;
-
-    // $scope.awesomeThings = [];
-
-    // $http.get('/api/things').success(function(awesomeThings) {
-    //   $scope.awesomeThings = awesomeThings;
-    //   socket.syncUpdates('thing', $scope.awesomeThings);
-    // });
-
-    // $scope.addThing = function() {
-    //   if($scope.newThing === '') {
-    //     return;
-    //   }
-    //   $http.post('/api/things', { name: $scope.newThing });
-    //   $scope.newThing = '';
-    // };
-
-    // $scope.deleteThing = function(thing) {
-    //   $http.delete('/api/things/' + thing._id);
-    // };
-
-    // $scope.$on('$destroy', function () {
-    //   socket.unsyncUpdates('thing');
-    // });
   });
